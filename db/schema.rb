511c4e340b1589421b96951c466bfc479e091436
John Costa <jcosta@execonline.com>
2016-03-19 11:05:51 -0700

    added role column to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805174831) do
+ActiveRecord::Schema.define(version: 20160319180502) do
 
   create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
 end
