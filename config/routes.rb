3e555f993de85196668d4cca101213a060be59b4
John Costa <jcosta@execonline.com>
2016-03-19 11:47:20 -0700

    fixed merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create]
   resources :sessions, only: [:create]
-<<<<<<< HEAD
   resources :answers
-  resources :questions
-=======
   resources :questions, only: [:new, :create]
->>>>>>> e23c357e86a88d8e00888ee063bd9f98118bd916
 
 end
