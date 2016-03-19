b332fd039e9c393b7561bd15974b2ee2b74353ff
John Costa <jcosta@execonline.com>
2016-03-19 11:44:14 -0700

    added model associations, controller for answers, routes, for questions and answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,6 @@ class User < ActiveRecord::Base
     }
 
   has_many :questions
+  has_many :answers
+  has_many :ratings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:create]
   resources :sessions, only: [:create]
+  resources :answers
+  resources :questions
 
 end
