37c9edeaa79e0a17c457a04bbaca18241961460e
ikiwi <kalai.wei@gmail.com>
2016-03-19 15:58:33 -0700

    fixed db

--- db/migrate/20160319220110_rename_column.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumn < ActiveRecord::Migration
-  def change
-    rename_column :answers, :question_id, :integer
-  end
-end
--- db/migrate/20160319220339_rename_userid_to_email.rb
+++
@@ -1,7 +0,0 @@
-class RenameUseridToEmail < ActiveRecord::Migration
-  def change
-    rename_colume :answers do |t|
-      t.rename :user_id, :email, :string
-    end
-  end
-end
