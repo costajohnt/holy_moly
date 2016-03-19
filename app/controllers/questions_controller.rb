3b2a4eefde53627c0a53cafe9c6f05da87cde6b4
Ilya Katz <ilyakatz@gmail.com>
2016-03-19 13:59:49 -0700

    style up creationg of questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -10,20 +10,15 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    if current_user.role = 'patient'
-      # redirect user if already logged in
-      @question = Question.new(question_params)
-      @question.user_id = current_user.id
-      if @question.save
-        flash[:notice] = "Question saved."
-      else
-        flash[:error] = @question.errors.full_messages.join(', ')
-      end
-      # redirect_to "/profile"
-      # refactored with route helpers:
-      redirect_to profile_path
+    # redirect user if already logged in
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    if @question.save
+      flash[:notice] = "Question saved."
+    else
+      flash[:error] = @question.errors.full_messages.join(', ')
     end
-    render :show
+    redirect_to user_questions_path(current_user.id)
   end
 
   def show
