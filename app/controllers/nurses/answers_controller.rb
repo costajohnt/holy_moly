02c6bc0d3976a626d0e63ace5803cfd03022da93
John Costa <jcosta@execonline.com>
2016-03-19 15:09:47 -0700

    question show page will now display all answers

--- app/controllers/nurses/answers_controller.rb
+++ app/controllers/nurses/answers_controller.rb
@@ -3,7 +3,7 @@ module Nurses
 
     def index
       @question = Question.find(params[:question_id])
-      @answers = Answer.where
+      @answers = Answer.all
     end
 
     def new
@@ -18,36 +18,6 @@ module Nurses
       if @answer.save
         @question.answers << @answer
         redirect_to nurses_question_answers_path
-      end
-    end
-
-    def edit
-      if current_user.role = 'nurse'
-        id = params[:id]
-        @answer = Answer.find(id)
-        render :edit #PSUEDO CODE ROUTE
-      end
-    end
-
-    def update
-      if current_user.role = 'nurse'
-        answer_id = params[:id]
-        answer = Answer.find(answer_id)
-        updated_attributes = params.require(:answer).permit(:content)
-        answer.updated_attributes(updated_attributes)
-        render 'question#show'  #This is PSEUDO CODE
-      end
-    end
-
-    def show
-    end
-
-    def delete
-      if current_user.role = 'nurse'
-        id = params[:id]
-        answer = Answer.find(id)
-        answer.destroy
-        render 'question#show'  #This is PSEUDO CODE
       end
     end
 
